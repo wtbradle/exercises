45f12216cd3ca8787b182ce88d92f036a251509e
wtbradle <wtbradle@ncsu.edu>
2015-02-07 17:22:30 -0500

    found a model (LASSO to GLM)
    need to get test scoring and chart

--- analysis.R
+++ analysis.R
@@ -59,6 +59,10 @@ mean(data$over_50k)
 
 summary(data$age)
 hist(data$age)
+par(mfrow=c(2,1))
+hist(data$age[data$over_50k==0])
+hist(data$age[data$over_50k==1])
+par(mfrow=c(1,1))
 
 summary(data$education_num)
 hist(data$education_num)
@@ -83,12 +87,17 @@ plot(data$capital_loss,data$capital_gain,col=c("black","red")[data$over_50k+1])
 summary(data$hours_week)
 hist(data$hours_week)
 # no zeros?  Even for unemployeed?
+par(mfrow=c(2,1))
+hist(data$hours_week[data$over_50k==0])
+hist(data$hours_week[data$over_50k==1])
+par(mfrow=c(1,1))
 boxplot(data$hours_week ~ data$occupation)
 boxplot(data$hours_week ~ data$class)
 # That's a lot of hours for never-worked
 table(data$occupation,data$over_50k)
 # Might need to drop some of these for small sample size
 # Looks like ? is a decent indicator for under 50k
+prop.table(table(data$occupation,data$over_50k),1)
 table(data$class,data$over_50k)
 # Toss never-worked
 data<-data[data$class!="Never-worked",]
@@ -188,6 +197,41 @@ pred2<-prediction(preds,dataValid$over_50k)
 perf2<-performance(pred2,"tpr","fpr")
 plot(perf)
 plot(perf2,add=TRUE,col="red")
+plot(perf3,add=TRUE,col="green")
+
+# use glm with lasso selection
+vars<-names(as.data.frame(x))[as.vector(coef(modelLog2)[,37])[-1]!=0]
+xNew <- as.data.frame(x)
+names(xNew)
+xNew<-xNew[,names(xNew)%in%vars]
+head(xNew)
+dataNew<-cbind(xNew,over_50k=dataTrain$over_50k)
+names(dataNew)
+modelLog3<-glm(over_50k ~ .,data=dataNew,family="binomial")
+
+toStandardize<-sapply(data,function(x)!is.factor(x))
+toStandardize<-dataValid[,names(dataValid)[toStandardize]]
+standVal<-scale(toStandardize,center=attr(stand,"scaled:center"),scale=attr(stand,"scaled:scale"))
+head(standVal)
+dataValid2<-dataValid[,names(dataValid)[sapply(data,function(x)is.factor(x))]]
+dataValid2<-cbind(dataValid2,data.frame(standVal))
+head(dataValid2)
+xValid <- model.matrix(over_50k ~ .,data=dataValid2)[,-1]
+head(xValid)
+xNewValid <- as.data.frame(xValid)
+xNewValid<-xNewValid[,names(xNewValid)%in%vars]
+head(xNewValid)
+dataNewValid<-cbind(xNewValid,over_50k=dataValid$over_50k)
+names(dataNewValid)
+
+preds<-predict(modelLog3,newdata=dataNewValid,type='response')
+head(preds)
+
+pred3<-prediction(preds,dataValid$over_50k)
+perf3<-performance(pred3,"tpr","fpr")
+plot(perf)
+plot(perf2,add=TRUE,col="red")
+plot(perf3,add=TRUE,col="green")
 # Not to shabby
 
 # Random forest, just because
@@ -198,17 +242,18 @@ plot(modelRF)
 preds<-predict(modelRF,newdata=dataValid,type="prob")
 head(preds)
 
-pred3<-prediction(preds[,2],dataValid$over_50k)
-perf3<-performance(pred3,"tpr","fpr")
+pred4<-prediction(preds[,2],dataValid$over_50k)
+perf4<-performance(pred4,"tpr","fpr")
 plot(perf)
 plot(perf2,add=TRUE,col="red")
-plot(perf3,add=TRUE,col="blue")
+plot(perf3,add=TRUE,col="green")
+plot(perf4,add=TRUE,col="blue")
 # Not as good
 
-# LASSO is best
+# GLM with Lasso selection is best
 
 # Get cutoff
-preds<-predict(modelLog2,newx=xValid,type='response',s=modelLog2$lambda[37])
+preds<-predict(modelLog3,newdata=dataNewValid,type='response')
 head(preds)
 compare<-data.frame("actual"=dataValid$over_50k,preds=as.vector(preds))
 head(compare)
@@ -229,6 +274,101 @@ cutoffs[misclasses==min(misclasses)]
 # .5 looks good
 
 # Now to get testing dataset
+#  Rebuild model
+
+
+
+# Start back here
+
+
+
+
+toStandardize<-sapply(data,function(x)!is.factor(x))
+toStandardize<-dataTrain[,names(dataTrain)[toStandardize]]
+stand<-scale(toStandardize)
+head(stand)
+dataTrain2<-dataTrain[,names(dataTrain)[sapply(data,function(x)is.factor(x))]]
+dataTrain2<-cbind(dataTrain2,stand)
+head(dataTrain2)
+x <- model.matrix(over_50k ~ .,data=dataTrain2)[,-1]
+head(x)
+y<-as.matrix(dataTrain2$over_50k)
+modelLog2<-glmnet(x,y,alpha=1,family='binomial')
+modelLog2
+plot(modelLog2,xvar="lambda")
+# 25 variables?
+abline(v=log(modelLog2$lambda[37]))
+
+names(as.data.frame(x))[as.vector(coef(modelLog2)[,37])[-1]!=0]
+toStandardize<-sapply(data,function(x)!is.factor(x))
+toStandardize<-dataValid[,names(dataValid)[toStandardize]]
+standVal<-scale(toStandardize,center=attr(stand,"scaled:center"),scale=attr(stand,"scaled:scale"))
+head(standVal)
+dataValid2<-dataValid[,names(dataValid)[sapply(data,function(x)is.factor(x))]]
+dataValid2<-cbind(dataValid2,data.frame(standVal))
+head(dataValid2)
+xValid <- model.matrix(over_50k ~ .,data=dataValid2)[,-1]
+head(xValid)
+
+preds<-predict(modelLog2,newx=xValid,type='response',s=modelLog2$lambda[37])
+head(preds)
+
+pred2<-prediction(preds,dataValid$over_50k)
+perf2<-performance(pred2,"tpr","fpr")
+plot(perf)
+plot(perf2,add=TRUE,col="red")
+plot(perf3,add=TRUE,col="green")
+
+# use glm with lasso selection
+vars<-names(as.data.frame(x))[as.vector(coef(modelLog2)[,37])[-1]!=0]
+xNew <- as.data.frame(x)
+names(xNew)
+xNew<-xNew[,names(xNew)%in%vars]
+head(xNew)
+dataNew<-cbind(xNew,over_50k=dataTrain$over_50k)
+names(dataNew)
+modelLog3<-glm(over_50k ~ .,data=dataNew,family="binomial")
+
+toStandardize<-sapply(data,function(x)!is.factor(x))
+toStandardize<-dataValid[,names(dataValid)[toStandardize]]
+standVal<-scale(toStandardize,center=attr(stand,"scaled:center"),scale=attr(stand,"scaled:scale"))
+head(standVal)
+dataValid2<-dataValid[,names(dataValid)[sapply(data,function(x)is.factor(x))]]
+dataValid2<-cbind(dataValid2,data.frame(standVal))
+head(dataValid2)
+xValid <- model.matrix(over_50k ~ .,data=dataValid2)[,-1]
+head(xValid)
+xNewValid <- as.data.frame(xValid)
+xNewValid<-xNewValid[,names(xNewValid)%in%vars]
+head(xNewValid)
+dataNewValid<-cbind(xNewValid,over_50k=dataValid$over_50k)
+names(dataNewValid)
+
+preds<-predict(modelLog3,newdata=dataNewValid,type='response')
+head(preds)
+
+
+
+# Score the test
+
+data2<-data[parts=="test",]
+toStandardize<-sapply(data,function(x)!is.factor(x))
+toStandardize<-data2[,names(data2)[toStandardize]]
+standVal<-scale(toStandardize,center=attr(stand,"scaled:center"),scale=attr(stand,"scaled:scale"))
+head(standVal)
+data2.2<-data2[,names(data2)[sapply(data,function(x)is.factor(x))]]
+data2.2<-cbind(data2.2,data.frame(standVal))
+head(data2.2)
+x2 <- model.matrix(over_50k ~ .,data=data2.2)[,-1]
+head(x2)
+xNew2 <- as.data.frame(x2)
+xNew2<-xNew2[,names(xNew2)%in%vars]
+head(xNew2)
+dataNew2<-cbind(xNew2,over_50k=data2$over_50k)
+names(dataNew2)
+
+preds<-predict(modelLog3,newdata=dataNewValid,type='response')
+head(preds)
 
 
 
